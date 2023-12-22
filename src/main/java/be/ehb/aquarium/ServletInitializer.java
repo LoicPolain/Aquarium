34e3f6fc612fb866ad340fc256674f41bbf2aa15
LoicPolain <loic.polain@student.ehb.be>
2023-12-22 13:24:51 +0100

    Init

---
+++ src/main/java/be/ehb/aquarium/ServletInitializer.java
@@ -0,0 +1,13 @@
+package be.ehb.aquarium;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AquariumApplication.class);
+    }
+
+}
---
+++ src/test/java/be/ehb/aquarium/AquariumApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.aquarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
